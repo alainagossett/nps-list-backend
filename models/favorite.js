644dc303aef7c7eefa5017bb2ea9171fd53a16ca
Alaina <alaina.gossett@gmail.com>
2022-01-18 19:14:56 -0700

    wip: adds favorite model

---
+++ models/favorite.js
@@ -0,0 +1,15 @@
+//Require Dependencies
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+//Define Schema
+const favoriteSchema = new Schema({
+    userId: { type: String, required: true },
+    parkId: String
+}, { timestamps: true });
+
+//Compile mongoose Schema into a model
+const Favorite = mongoose.model("Favorite", favoriteSchema);
+
+module.exports = Favorite;
