5a66349f23607015dcb3fe358ca8e60b205437a6
Alaina <alaina.gossett@gmail.com>
2022-01-29 13:22:33 -0700

    adds authorization to routes

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -1,11 +1,28 @@
 //Require Dependencies
 const express = require('express');
-const Favorite = require('../models/favorite');
 const admin = require('firebase-admin');
+
+const Favorite = require('../models/favorite');
 
 const favoritesRouter = express.Router();
 
 require('dotenv').config();
+
+async function isAuthenticated(req, res, next) {
+    try {
+        const token = req.get('Authorization');
+        if(!token) throw new Error('You must be logged in first');
+        
+        const user = await admin.auth().verifyIdToken(token.replace('Bearer ', ''));
+        if(!user) throw new Error('Something went wrong');
+
+        req.user = user;
+
+        next();
+    } catch(error) {
+        res.status(400).json({ message: error.message });
+    }
+} 
 
 //Define Routes
 favoritesRouter.get('/', (req, res) => {
@@ -13,21 +30,18 @@ favoritesRouter.get('/', (req, res) => {
 });
 
 //INDEX ROUTE
-favoritesRouter.get('/favorites', async (req, res) => {
+favoritesRouter.get('/favorites', isAuthenticated, async (req, res) => {
     try {
-        res.json(await Favorite.find({}))
+        res.json(await Favorite.find({uId: req.user.uid}))
     } catch (error) {
         res.status(400).json(error)
     }
 });
 
 //CREATE ROUTE
-favoritesRouter.post('/favorites', async (req, res) => {
-    const token = req.get('Authorization');
-    if(!token) return res.status(400).json({message: 'You must be logged in first'});
-    const user = await admin.auth().verifyIdToken(token.replace('Bearer ', ''));
-    req.body.uId = user.uid;
+favoritesRouter.post('/favorites', isAuthenticated, async (req, res) => {
     try {
+        req.body.uId = req.user.uid;
         res.json(await Favorite.create(req.body))
     } catch (error) {
         res.status(400).json(error)
